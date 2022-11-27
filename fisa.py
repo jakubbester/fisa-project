9b6a5466f0671bcfa90fa66bf215a94c34882423
Jakub Bester <jakubbester@yahoo.com>
2022-11-27 14:26:12 -0500

    form with updating info

--- fisa.py
+++ fisa.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import *
 from z3 import *
 
 # HANDLE LOGIC
-Tie, Shirt = Bools('Tie Shirt')
-s = Solver()
-s.add(Or(Tie, Shirt), Or(Not(Tie), Shirt), Or(Not(Tie), Not(Shirt)))
-print(s.check())
-print(s.model())
+def logic_handler(request):
+    # Tie, Shirt = Bools('Tie Shirt')
+    # s = Solver()
+    # s.add(Or(Tie, Shirt), Or(Not(Tie), Shirt), Or(Not(Tie), Not(Shirt)))
+    return True
 
 # CREATE FLASK APP
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=["GET", "POST"])
+def index():
+    result = None
+    if request.method == "POST":
+        result = logic_handler(request)
+
+    return render_template("index.html", data=result)
 
 if __name__ == "__main__":
     app.run()
