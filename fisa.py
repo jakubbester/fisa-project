ba47715372a252cc47476f1c1f64c13d899dc239
Jakub Bester <jakubbester@yahoo.com>
2022-11-27 20:22:58 -0500

    fixes

--- fisa.py
+++ fisa.py
@@ -1,20 +1,27 @@
 from flask import *
 from z3 import *
 
+NAMES = [
+    "consent",
+]
+
 # HANDLE LOGIC
 def logic_handler(request):
+    # FILE MANIPULATION
     data1 = None; data2 = None
     with open("50 USC 1801 (f)-z3-tmp1.txt", "r") as file:
         data1 = file.read()
-        data2 = data1.replace("check-sat", "check-sat1")
+        print(request.form.get("consent"))
+        data2 = data1.replace("check-sat", "check-sat1") # replace placeholders
     
     with open("50 USC 1801 (f)-z3-tmp2.txt", "w") as file:
         file.write(data2)
 
+    # USE Z3 SOLVER ACORDINGLY
     s = Solver()
-    print(s.from_file("50 USC 1801 (f)-z3-tmp1.txt"))
-    print(s.check())
-    print(s.model())
+    s.from_file("50 USC 1801 (f)-z3-tmp1.txt")
+    # print(s.check())
+    # print(s.model())
 
     return True
 
