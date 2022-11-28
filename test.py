444d1ae3177e2468c989cad1ab64342defbbf055
Jakub Bester <jakubbester@yahoo.com>
2022-11-27 20:25:29 -0500

    change

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from z3 import *
 
 if __name__ == "__main__":
     s = Solver()
-    s.from_file("50 USC 1801 (f)-z3.txt")
+    s.from_file("50 USC 1801 (f)-z3-tmp1.txt")
     print(s.check())
     print(s.model())
